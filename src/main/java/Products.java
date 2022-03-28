27a8e9ab42f8e60751292e47f778a93d7e6b1cec
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-28 11:19:16 -0500

    created the interface for DAO

---
+++ src/main/java/Products.java
@@ -0,0 +1,9 @@
+// TODO: This interface describes the operations that our app can perform with products
+
+import java.util.List;
+
+interface Products {
+    List<Product> all(); // get all the product records
+
+    void insert(Product product); // persist new product to the database
+}
