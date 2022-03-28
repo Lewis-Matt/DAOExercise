19d680307d54ed8c9324f1fef050bdb4d9d7df61
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-28 14:37:44 -0500

    created and finished servlet

---
+++ src/main/java/CreateProductServlet.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/products/create")
+public class CreateProductServlet extends HttpServlet {
+    @Override
+    // Shows the corresponding JSP file
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/products/create.jsp").forward(request, response);
+    }
+
+    @Override
+    // Access the values the user submitted, and use those values to create a new Product object
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Products productsDao = DaoFactory.getProductsDao();
+        // Create a new product based upon submitted data
+        String name = request.getParameter("name");
+        // Parse as all values we obtain from the user are strings
+        // The string argument passed to request.getParameter must match up with the name attribute defined on the inputs in our HTML.
+        double price = Double.parseDouble(request.getParameter("price"));
+        Product product = new Product(name, price);
+        // Persist the new product
+        productsDao.insert(product);
+        response.sendRedirect("/products");
+    }
+}
